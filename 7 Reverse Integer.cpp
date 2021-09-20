6a008c1c4c636b7cadf067662f1b3a3cc79c7e49
Ankita Puri <puriankita30@gmail.com>
2021-09-20 21:29:14 +0530

    solved

---
+++ 7 Reverse Integer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long int ans = 0;
+        while(x!=0)
+        {
+            ans = ans*10 + x%10;
+            x/=10;
+        }
+        if(ans>INT_MAX || ans<INT_MIN) return 0;
+        return ans;
+    }
+};
